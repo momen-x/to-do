1ae09879621a9c07811323fe24bca4e20fcb6be0
Mo'men <mmm.isam.99@gmail.com>
2025-07-14 11:59:53 +0200

    v4, mack costom hook by Combining Reducer With Context to access to the tasks array from all componnent children

--- src/Context/ListIfTaskContext.jsx
+++ src/Context/ListIfTaskContext.jsx
@@ -1,15 +1,36 @@
-import { createContext,useReducer,useContext } from "react";
-import { ListOfReducer } from "./reducer/ListOfTask.js";
+import { createContext, useReducer, useContext, useEffect } from "react";
+import { ListOfReducer } from "../reducer/ListOfTask.js";
 
- const ListOfContext=createContext([]);
+const ListOfContext = createContext([]);
 
- export function ListOfTaskProvider({ children }) {
-    const [tasks, dispatch] = useReducer(ListOfReducer, JSON.parse(localStorage.getItem("toDoList")) || []);
 
-    return (
-    <ListOfContext.Provider value={{tasks, dispatch}}>
+
+function ListOfTaskProvider({ children }) {
+  const [tasks, dispatch] = useReducer(ListOfReducer, []);
+
+  // Sync state to localStorage whenever tasks change
+  useEffect(() => {
+    try {
+      localStorage.setItem("toDoList", JSON.stringify(tasks));
+    } catch (err) {
+      console.error("Failed to save tasks to localStorage", err);
+    }
+  }, [tasks]);
+
+  return (
+    <ListOfContext.Provider value={{ tasks, dispatch }}>
       {children}
     </ListOfContext.Provider>
   );
-
 }
+
+function useListOfTask() {
+  const context = useContext(ListOfContext);
+  if (!context) {
+    throw new Error("useListOfTask must be used within a ListOfTaskProvider");
+  }
+  return context;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { ListOfContext, ListOfTaskProvider, useListOfTask };
